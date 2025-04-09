a6efadaa679b1284c317c4dded147c73a6ea34fd
reimi <r.yamada013@icloud.com>
2025-04-09 13:34:38 +0900

    ダミーデータ作成

--- src/database/seeders/CategoryItemSeeder.php
+++ src/database/seeders/CategoryItemSeeder.php
@@ -15,14 +15,31 @@ class CategoryItemSeeder extends Seeder
     public function run()
     {
         DB::table('category_item')->insert([
-            [
-                'category_id' => 1,
-                'item_id' => 1,
-            ],
-            [
-                'category_id' => 1,
-                'item_id' => 2,
-            ],
+            ['category_id' => 1, 'item_id' => 1],
+            ['category_id' => 1, 'item_id' => 4],
+            ['category_id' => 1, 'item_id' => 7],
+            ['category_id' => 1, 'item_id' => 10],
+            ['category_id' => 2, 'item_id' => 2],
+            ['category_id' => 2, 'item_id' => 5],
+            ['category_id' => 2, 'item_id' => 6],
+            ['category_id' => 3, 'item_id' => 9],
+            ['category_id' => 4, 'item_id' => 5],
+            ['category_id' => 4, 'item_id' => 7],
+            ['category_id' => 4, 'item_id' => 10],
+            ['category_id' => 5, 'item_id' => 1],
+            ['category_id' => 5, 'item_id' => 4],
+            ['category_id' => 5, 'item_id' => 5],
+            ['category_id' => 5, 'item_id' => 8],
+            ['category_id' => 6, 'item_id' => 10],
+            ['category_id' => 8, 'item_id' => 2],
+            ['category_id' => 8, 'item_id' => 5],
+            ['category_id' => 8, 'item_id' => 6],
+            ['category_id' => 10, 'item_id' => 3],
+            ['category_id' => 10, 'item_id' => 8],
+            ['category_id' => 10, 'item_id' => 9],
+            ['category_id' => 11, 'item_id' => 3],
+            ['category_id' => 12, 'item_id' => 1],
+            ['category_id' => 12, 'item_id' => 7],
         ]);
     }
 }
