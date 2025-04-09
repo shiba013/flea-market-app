f7ae8a6c29cac8d1c8465f96e7638ad887c9d918
reimi <r.yamada013@icloud.com>
2025-04-09 13:13:54 +0900

    update tables

---
+++ src/database/seeders/ItemsSeeder.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ItemsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('items')->updateOrInsert([
+            [
+                'user_id' => 1,
+                'condition_id' => 1,
+                'name' => '腕時計',
+                'price' => 15000,
+                'description' => 'スタイリッシュなデザインのメンズ腕時計',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Armani+Mens+Clock.jpg',
+            ],
+            [
+                'user_id' => 1,
+                'condition_id' => 2,
+                'name' => 'HDD',
+                'price' => 5000,
+                'description' => '高速で信頼性の高いハードディスク',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/HDD+Hard+Disk.jpg',
+            ],
+            [
+                'user_id' => 1,
+                'condition_id' => 3,
+                'name' => '玉ねぎ3束',
+                'price' => 300,
+                'description' => '新鮮な玉ねぎ3束のセット',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/iLoveIMG+d.jpg',
+            ],
+            [
+                'user_id' => 1,
+                'condition_id' => 4,
+                'name' => '革靴',
+                'price' => 4000,
+                'description' => 'クラシックなデザインの革靴',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Leather+Shoes+Product+Photo.jpg',
+            ],
+            [
+                'user_id' => 1,
+                'condition_id' => 1,
+                'name' => 'ノートPC',
+                'price' => 45000,
+                'description' => '高性能なノートパソコン',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Living+Room+Laptop.jpg',
+            ],
+            [
+                'user_id' => 1,
+                'condition_id' => 2,
+                'name' => 'マイク',
+                'price' => 8000,
+                'description' => '高音質のレコーディング用マイク',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Music+Mic+4632231.jpg',
+            ],
+            [
+                'user_id' => 1,
+                'condition_id' => 3,
+                'name' => 'ショルダーバッグ',
+                'price' => 3500,
+                'description' => 'おしゃれなショルダーバッグ',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Purse+fashion+pocket.jpg',
+            ],
+            [
+                'user_id' => 1,
+                'condition_id' => 4,
+                'name' => 'タンブラー',
+                'price' => 500,
+                'description' => '使いやすいタンブラー',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Tumbler+souvenir.jpg',
+            ],
+            [
+                'user_id' => 1,
+                'condition_id' => 1,
+                'name' => 'コーヒーミル',
+                'price' => 4000,
+                'description' => '手動のコーヒーミル',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Waitress+with+Coffee+Grinder.jpg',
+            ],
+            [
+                'user_id' => 1,
+                'condition_id' => 2,
+                'name'=> 'メイクセット',
+                'price' => 2500,
+                'description' => '便利なメイクアップセット',
+                'image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/%E5%A4%96%E5%87%BA%E3%83%A1%E3%82%A4%E3%82%AF%E3%82%A2%E3%83%83%E3%83%95%E3%82%9A%E3%82%BB%E3%83%83%E3%83%88.jpg',
+            ],
+        ]);
+    }
+}
