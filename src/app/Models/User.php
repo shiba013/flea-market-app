0e3e21c3ea0fd052c1a840e3e68f5d27ab24f295
reimi <r.yamada013@icloud.com>
2025-05-14 15:35:24 +0900

    メール認証設定

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
@@ -25,6 +25,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'post_code',
         'address',
         'building',
+        'email_verified_at',
     ];
 
     public function items()
